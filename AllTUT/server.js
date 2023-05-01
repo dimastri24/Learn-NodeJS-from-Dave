1b928c822a7e0b0e93e033ee78584e122fcb1958
Dimas Tri Raharjo <dimastriraharjo24@gmail.com>
2023-05-01 18:00:08 +0700

    middleware in express js

--- AllTUT/server.js
+++ AllTUT/server.js
@@ -1,11 +1,42 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const cors = require('cors');
+const { logger } = require('./middleware/logEvents');
+const errorHandler = require('./middleware/errorHandler');
 
 const PORT = process.env.PORT || 3500;
 
+// custom middleware logger
+app.use(logger);
+
+// cross origin resource sharing
+// whitlisting the domain
+const whiteList = ['https://www.yoursite.com', 'http://127.0.0.1:5500', 'http://localhost:3500'];
+const corsOptions = {
+    origin: (origin, callback) => {
+        if (whiteList.indexOf(origin) !== -1 || !origin) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
+
+// built-in middleware to handle urlendcoded data
+// in other words, form data
+// 'content-type: application/x-www-form-urlendcoded'
+app.use(express.urlencoded({ extended: false }));
+
+// built-in middleware for json
+app.use(express.json());
+
+// serve static files
+app.use(express.static(path.join(__dirname, '/public')));
+
 app.get('^/$|/index(.html)?', (req, res) => {
-    // res.sendFile('./views/index.html', { root: __dirname });
     res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
 
@@ -45,8 +76,23 @@ const three = (req, res) => {
 
 app.get('/chain(.html)?', [one, two, three]);
 
-app.get('/*', (req, res) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+// app.get('/*', (req, res) => {
+//     res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+// });
+// 404 as middleware
+// use() doesn't accept regex, instead use all() although all() not a middleware function
+app.all('*', (req, res) => {
+    res.status(404);
+    if (req.accepts('html')) {
+        res.sendFile(path.join(__dirname, 'views', '404.html'));
+    } else if (req.accepts('json')) {
+        res.json({ error: '404 Not Found' });
+    } else {
+        res.type('txt').send('404 Not Found');
+    }
 });
+
+// custom error cors
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
