5f81186d368a11607e5b36e64aad14321173b649
Dimas Tri Raharjo <dimastriraharjo24@gmail.com>
2023-05-02 10:14:20 +0700

    express router with rest api routing

--- AllTUT/server.js
+++ AllTUT/server.js
@@ -34,51 +34,14 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // serve static files
-app.use(express.static(path.join(__dirname, '/public')));
+app.use('/', express.static(path.join(__dirname, '/public')));
+app.use('/subdir', express.static(path.join(__dirname, '/public')));
 
-app.get('^/$|/index(.html)?', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'index.html'));
-});
+// call Router
+app.use('/', require('./routes/root'));
+app.use('/subdir', require('./routes/subdir'));
+app.use('/employees', require('./routes/api/employees'));
 
-app.get('/new-page(.html)?', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'new-page.html'));
-});
-
-app.get('/old-page(.html)?', (req, res) => {
-    res.redirect(301, 'new-page.html'); // send 302 by default
-});
-
-// Route handlers
-app.get(
-    '/hello(.html)?',
-    (req, res, next) => {
-        console.log('attempt to load hello.html');
-        next();
-    },
-    (req, res) => {
-        res.send('Hello World!!!!');
-    }
-);
-
-// chaining route handlers
-const one = (req, res, next) => {
-    console.log('one');
-    next();
-};
-const two = (req, res, next) => {
-    console.log('two');
-    next();
-};
-const three = (req, res) => {
-    console.log('three');
-    res.send('Finished!');
-};
-
-app.get('/chain(.html)?', [one, two, three]);
-
-// app.get('/*', (req, res) => {
-//     res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
-// });
 // 404 as middleware
 // use() doesn't accept regex, instead use all() although all() not a middleware function
 app.all('*', (req, res) => {
