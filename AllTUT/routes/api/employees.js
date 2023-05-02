2c92292264d805c0425c129be484e8ef74f27812
Dimas Tri Raharjo <dimastriraharjo24@gmail.com>
2023-05-02 16:56:40 +0700

    jwt authentication

--- AllTUT/routes/api/employees.js
+++ AllTUT/routes/api/employees.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const employeesController = require('../../controllers/employeeController');
+// const verifyJWT = require('../../middleware/verifyJWT');
 
 router
     .route('/')
+    // .get(verifyJWT, employeesController.getAllEmployee) // put middleware jwt one by one
     .get(employeesController.getAllEmployee)
     .post(employeesController.createNewEmployee)
     .put(employeesController.updateEmployee)
