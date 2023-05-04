7237bf29ed04f50800e4a4d48961d56f0aaf3636
Dimas Tri Raharjo <dimastriraharjo24@gmail.com>
2023-05-04 16:32:05 +0700

    Revert "rt rotation and detection"
    This reverts commit f801b96a866637994eb4ddab4802ac6cf0da4572.

--- AllTUT/controllers/logoutController.js
+++ AllTUT/controllers/logoutController.js
@@ -1,25 +1,25 @@
 const User = require('../model/User');
 
 const handleLogout = async (req, res) => {
-    // On client, also delete the accessToken
+    // On client also delete the accessToken
 
     const cookies = req.cookies;
-    if (!cookies?.jwt) return res.sendStatus(204); //No content
+    if (!cookies?.jwt) return res.sendStatus(204); // no content
     const refreshToken = cookies.jwt;
 
     // Is refreshToken in db?
     const foundUser = await User.findOne({ refreshToken }).exec();
     if (!foundUser) {
         res.clearCookie('jwt', { httpOnly: true, sameSite: 'None', secure: true });
-        return res.sendStatus(204);
+        return res.sendStatus(204); // no content
     }
 
     // Delete refreshToken in db
-    foundUser.refreshToken = foundUser.refreshToken.filter((rt) => rt !== refreshToken);
+    foundUser.refreshToken = '';
     const result = await foundUser.save();
     console.log(result);
 
-    res.clearCookie('jwt', { httpOnly: true, sameSite: 'None', secure: true });
+    res.clearCookie('jwt', { httpOnly: true, sameSite: 'None', secure: true }); // secure: true - only serves on https
     res.sendStatus(204);
 };
 
--- AllTUT/middleware/credentials.js
+++ AllTUT/middleware/credentials.js
@@ -3,7 +3,7 @@ const allowedOrigins = require('../config/allowedOrigins');
 const credentials = (req, res, next) => {
     const origin = req.headers.origin;
     if (allowedOrigins.includes(origin)) {
-        res.header('Access-Control-Allow-Credentials', true);
+        res.headers('Access-Control-Allow-Credentials', true);
     }
     next();
 };
